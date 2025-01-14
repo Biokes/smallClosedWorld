704ecb3430d82885f0addb89fb562d25bdc2d0c4
Biokes <106444765+Biokes@users.noreply.github.com>
2025-01-14 16:41:16 +0100

    update urls, views

--- users_module/urls.py
+++ users_module/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from users_module.views import RegisterView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from users_module.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('api/register/', RegisterView.as_view(), name='register-user'),
+    path('/', include(router.urls)),
+    path('register/', UserViewSet.as_view({'post': 'create'}), name='register-user'),
 ]
--- users_module/views.py
+++ users_module/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from models import User
 from serializers import UserSerializer
+from rest_framework.decorators import action
 
-
-# Create your views here.
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer(partial=True, many=True)
+    serializer_class = UserSerializer(partial=True)
+
+    @action(detail=False, methods=['post'], url_path='register')
+    def create_user(self, request):
+        return self.create(request)
